ab78d12b5604534dc2b39d8a595600a2290835e8
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-06-04 16:30:12 +0300

    added dublicating

--- scripts/acco.js
+++ scripts/acco.js
@@ -1,21 +1,8 @@
-const getHeightOfBlock = (block) => block.height();
-
-const mobileQuery = window.matchMedia("(max-width: 768px)");
-
-const getContentBlockClass = () => {
-  const isMobile = mobileQuery.matches;
-
-  return isMobile
-    ? ".team__content--hidden--mobile"
-    : ".team__content--hidden--desktop";
-};
-
 const openItem = (item) => {
   const container = item.closest(".team__item");
-  const contentBlockClass = getContentBlockClass();
-  const contentBlock = container.find(contentBlockClass);
+  const contentBlock = container.find(".team__content");
   const textBlock = contentBlock.find(".team__content-block");
-  const reqHeight = getHeightOfBlock(textBlock);
+  const reqHeight = textBlock.height();
 
   container.addClass("active");
   contentBlock.height(reqHeight);
@@ -23,33 +10,11 @@ const openItem = (item) => {
 
 const closeEveryItem = (container) => {
   const items = container.find(".team__item");
-  const contentBlockClass = getContentBlockClass();
-  const contentBlock = container.find(contentBlockClass);
+  const contentBlock = container.find(".team__content");
 
   items.removeClass("active");
   contentBlock.height(0);
 };
-
-const resetElemsHeightsInContainer = (container) => {
-  const mobileContainer = container.find(".team__content--hidden--mobile");
-  const items = container.find(".team__item");
-  const desktopContainer = container.find(".team__content--hidden--desktop");
-  const isMobile = mobileQuery.matches;
-
-  if (isMobile) {
-    mobileContainer.height(0);
-    desktopContainer.height("auto");
-  } else {
-    mobileContainer.height("auto");
-    desktopContainer.height(0);
-  }
-
-  items.removeClass(".active");
-};
-
-mobileQuery.addListener((e) => {
-  resetElemsHeightsInContainer($(".team"));
-});
 
 $(".team__title").click((e) => {
   e.preventDefault();
