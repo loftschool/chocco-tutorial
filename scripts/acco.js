9fb36403c21d0ec7d1280c60d5e19283a5207a04
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-06-05 14:13:30 +0300

    acco-completed

--- scripts/acco.js
+++ scripts/acco.js
@@ -9,22 +9,19 @@ const openItem = (item) => {
 };
 
 const closeEveryItem = (container) => {
-  const items = container.find(".team__item");
-  const contentBlock = container.find(".team__content");
+  const items = container.find(".team__content");
+  const itemContainer = container.find(".team__item");
 
-  items.removeClass("active");
-  contentBlock.height(0);
+  itemContainer.removeClass("active");
+  items.height(0);
 };
 
 $(".team__title").click((e) => {
-  e.preventDefault();
-
   const $this = $(e.currentTarget);
-  const elemContainer = $this.closest(".team__item");
-  const itemOpened = elemContainer.hasClass("active");
   const container = $this.closest(".team");
+  const elemContainer = $this.closest(".team__item");
 
-  if (itemOpened) {
+  if (elemContainer.hasClass("active")) {
     closeEveryItem(container);
   } else {
     closeEveryItem(container);
