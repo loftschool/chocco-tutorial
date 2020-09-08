14a7b0cf99319c64645cb15f6e83d1d5a8eacc42
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-09-08 13:23:01 +0300

    added map

--- scripts/map.js
+++ scripts/map.js
@@ -12,13 +12,16 @@ const init = () => {
       [59.88693161784606, 30.319658102103713],
       [59.97033574821672, 30.315194906302924],
     ],
-    myCollection = new ymaps.GeoObjectCollection({}, {
-      draggable: false,
-      iconLayout: 'default#image',
-      iconImageHref: './img/icons/marker.svg',
-      iconImageSize: [46, 57],
-      iconImageOffset: [-35, -52]
-    });
+    myCollection = new ymaps.GeoObjectCollection(
+      {},
+      {
+        draggable: false,
+        iconLayout: "default#image",
+        iconImageHref: "./img/icons/marker.svg",
+        iconImageSize: [46, 57],
+        iconImageOffset: [-35, -52],
+      }
+    );
 
   for (let i = 0; i < coords.length; i++) {
     myCollection.add(new ymaps.Placemark(coords[i]));
@@ -26,7 +29,7 @@ const init = () => {
 
   myMap.geoObjects.add(myCollection);
 
-  myMap.behaviors.disable('scrollZoom');
+  myMap.behaviors.disable("scrollZoom");
 };
 
 ymaps.ready(init);
